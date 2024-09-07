9f821f741ceb89c4be2201bafa08727ae2847334
Ali Zaghloul Attia <90622060+ali22905@users.noreply.github.com>
2024-09-07 20:51:50 +0200

    Changed name in Navbar

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <img src="../assets/my-logo.png" alt="" style={{width: '100px'}} />
-        <h4 className="font-playfair text-3xl font-bold">Ali Zaghloul</h4>
+        <h4 className="font-playfair text-3xl font-bold">Ali Attia</h4>
 
         {/* DESKTOP NAV */}
         {isDesktop ? (
