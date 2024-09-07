0a31ca7c3e75a7a82a762dca52193c498eeb0153
Ali Zaghloul Attia <90622060+ali22905@users.noreply.github.com>
2024-09-07 20:49:37 +0200

    used the new ali picture

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -21,7 +21,7 @@ const Landing = ({ setSelectedPage }) => {
               alt="profile"
               style={{borderTopLeftRadius: "50%", borderTopRightRadius: '50%'}}
               className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-              src="assets/ali-image.jpg"
+              src="assets/ali.jpg"
             />
           </div>
         ) : (
